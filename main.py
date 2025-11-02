652439941d97ad6afa73cc736854ff740dd04e87
k.portnova <k.portnova@brusnika.ru>
2025-11-02 21:46:08 +0500

    feat: add linker configs

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-a = 1
-b = 2
-print(a + b)
-print("I am the conflict ma friend from the second branch")
-print("I don't like conflicts ma friend from the second branch")
-print('just check protection rule')
-print("I am making the conflict ma friend from the second branch")
-print("I don't like conflicts ma friend from the second branch")
+def greet(name: str) -> None:
+    print(f"Hello, {name}!")
 
 
-print('just check protection rule')
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+if __name__ == "__main__":
+    greet("Katerina")
+    print(add(5, 3))
