09790ba52b9c1f58ed85ce9a73d581738c557233
k.portnova <k.portnova@brusnika.ru>
2025-11-02 17:05:54 +0500

    test protection from second branch

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-print('I wanna smth')
-print("I am CREATING the conflict ma friend from the second branch")
+print("I am the conflict ma friend from the second branch")
 print("I don't like conflicts ma friend from the second branch")
-print('just check protection rule')
