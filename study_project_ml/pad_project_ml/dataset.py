881c426167d9d83de0fdbddb11a03c90d6707ebe
fuckseer <ksusha.magic.96@gmail.com>
2025-11-19 19:29:32 +0500

    removed duplicated code

--- study_project_ml/pad_project_ml/dataset.py
+++ study_project_ml/pad_project_ml/dataset.py
@@ -1,21 +1,9 @@
-import os
-import boto3
-from loguru import logger
 from pathlib import Path
 
-session = boto3.session.Session()
-s3 = session.client(
-    service_name='s3',
-    endpoint_url=os.getenv("S3_ENDPOINT_URL"),
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-)
+from study_project_ml.pad_project_ml.s3_utils import upload_to_s3
 
-RAW_BUCKET = os.getenv("RAW_BUCKET", "study-project-data")
 RAW_DATA_DIR = Path("data/raw")
 
-def upload_raw_dataset():
-    path = RAW_DATA_DIR / "titanic.csv"
-    logger.info(f"Uploading {path} → s3://{RAW_BUCKET}")
-    s3.upload_file(str(path), RAW_BUCKET, "titanic.csv")
-    logger.success("Upload complete.")
+def upload_raw_dataset() -> None:
+    file_path = RAW_DATA_DIR / "titanic.csv"
+    upload_to_s3(file_path, "titanic.csv")
